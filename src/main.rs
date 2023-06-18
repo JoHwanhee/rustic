a17360594412d396dd420da5fb0cdda768a643ae
johwanhui <kikiki0611@gmail.com>
2023-06-18 23:27:51 +0900

    refactor: handle_connection to be generic and add tests

--- src/main.rs
+++ src/main.rs
@@ -2,25 +2,59 @@ use std::io::prelude::*;
 use std::net::TcpListener;
 use std::net::TcpStream;
 
+
+fn handle_connection<R: Read, W: Write, F>(mut reader: R, mut writer: W, generate_response: F)
+    where
+        F: Fn(&[u8]) -> Vec<u8>,
+{
+    let mut buffer = [0; 1024];
+    reader.read(&mut buffer).unwrap();
+
+    let response = generate_response(&buffer);
+    writer.write(&response).unwrap();
+}
+
+fn generate_response(_request: &[u8]) -> Vec<u8> {
+    "HTTP/1.1 200 OK\r\n\r\nHello, World!".as_bytes().to_vec()
+}
+
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
 
     for stream in listener.incoming() {
-        let stream = stream.unwrap();
+        let mut stream = stream.unwrap();
+        let reader = stream.try_clone().unwrap();
 
-        handle_connection(stream);
+        handle_connection(reader, &mut stream, generate_response);
     }
 }
 
-fn handle_connection(mut stream: TcpStream) {
-    let mut buffer = [0; 1024];
+#[cfg(test)]
+mod tests {
+    use super::*;
 
-    stream.read(&mut buffer).unwrap();
+    use super::*;
+    use std::io::Cursor;
 
-    println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
+    #[test]
+    fn test_generate_response() {
+        let request = b"GET / HTTP/1.1\r\n\r\n";  // Example HTTP request
+        let response = generate_response(request);
 
-    let response = "HTTP/1.1 200 OK\r\n\r\nHello, World!";
+        let expected_response = "HTTP/1.1 200 OK\r\n\r\nHello, World!".as_bytes().to_vec();
 
-    stream.write(response.as_bytes()).unwrap();
-    stream.flush().unwrap();
+        assert_eq!(response, expected_response);
+    }
+
+    #[test]
+    fn test_handle_connection() {
+        let request = b"GET / HTTP/1.1\r\n\r\n";
+        let mut reader = Cursor::new(request);
+        let mut writer = Cursor::new(vec![]);
+
+        handle_connection(&mut reader, &mut writer, generate_response);
+
+        let response = writer.into_inner();
+        assert_eq!(response, b"HTTP/1.1 200 OK\r\n\r\nHello, World!");
+    }
 }
